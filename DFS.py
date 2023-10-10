72c8196e9ca51f7ea61f972a266f10873b23c9e3
Abdul Gaffar <137311996+Ghaffar-0604@users.noreply.github.com>
2023-10-10 15:31:11 +0530

    Add files via upload

--- DFS.py
+++ DFS.py
@@ -1,17 +1,21 @@
 graph={
-    'a':['b','c'],
-    'b':['d','e'],
-    'c':['f'],
-    'd':[],
-    'e':['f'],
-    'f':[],
-    }
-visited= set()
-def dfs (visited,graph,node):
+    'A':['B','C'],
+    'B':['D','E'],
+    'C':['F'],
+    'D':[],
+    'E':['F'],
+    'F':[],
+}
+visited = set() # set to keep track of visited nodes of graph.
+
+def dfs(visited,graph,node):
+    #print(visited)  #function for dfs
     if node not in visited:
-        print(node)
+        print (node)
         visited.add(node)
         for neighbour in graph[node]:
             dfs(visited,graph,neighbour)
-print("folloeing is thge depth first search")
-dfs(visited,graph,'a')
+
+
+print("Following is the Depth-First search")
+dfs(visited,graph,'A')
